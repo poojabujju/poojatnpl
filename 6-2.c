3ecfdf275081c0f632fb22317fe30208d3ef689d
poojabujju <31477611+poojabujju@users.noreply.github.com>
2018-01-08 11:27:07 +0000

    Create 6-2.c

---
+++ 6-2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int str;
+int a;
+printf("enter the string");
+scanf("%d",&str);
+if((str==0)||(str==1))
+{
+printf("binary representation");
+}
+else
+{
+printf("not a binary representation");
+}
+}
