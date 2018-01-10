e2f451a39b050c22672fab7284858824283f4193
poojabujju <31477611+poojabujju@users.noreply.github.com>
2018-01-10 11:06:22 +0000

    Create 7-8.c

---
+++ 7-8.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter a number");
+scanf("%d",&a);
+if(a%7==0)
+{
+printf("it is a multiple of 7");
+}
+else            
+{
+printf("it is not a multiple of 7");
+}
+}
